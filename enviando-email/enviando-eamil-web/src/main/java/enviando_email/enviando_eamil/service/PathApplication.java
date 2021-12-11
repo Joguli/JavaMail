9899beb2a0f2ce7a2eaf47b3372a5e908a29e5e7
Josué <vintetrezi@gmail.com>
2021-12-11 07:07:25 -0300

    Change.

--- enviando-email/enviando-eamil-web/src/main/java/enviando_email/enviando_eamil/service/PathApplication.java
+++
@@ -1,8 +0,0 @@
-package enviando_email.enviando_eamil.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("service")
-public class PathApplication extends Application {
-}
