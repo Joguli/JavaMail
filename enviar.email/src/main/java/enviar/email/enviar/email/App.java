6b7e3e4a62ea7172a74920793794dd6fe996f8ad
Josué <vintetrezi@gmail.com>
2021-12-11 09:02:14 -0300

    Project maves is ready.

---
+++ enviar.email/src/main/java/enviar/email/enviar/email/App.java
@@ -0,0 +1,13 @@
+package enviar.email.enviar.email;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ enviar.email/src/test/java/enviar/email/enviar/email/AppTest.java
@@ -0,0 +1,10 @@
+package enviar.email.enviar.email;
+
+public class AppTest 
+{
+	@org.junit.Test
+	public void testeEmail() {
+		//Olha as configurações do smtp do seu email.
+	}
+  
+}
