eb4ca3f7326552700b7ef7087877004602d63878
Josué <vintetrezi@gmail.com>
2021-12-12 07:13:12 -0300

    Setting connection properties.

--- enviar.email/src/test/java/enviar/email/enviar/email/AppTest.java
+++ enviar.email/src/test/java/enviar/email/enviar/email/AppTest.java
@@ -1,10 +1,21 @@
 package enviar.email.enviar.email;
 
+import java.util.Properties;
+
 public class AppTest 
 {
 	@org.junit.Test
 	public void testeEmail() {
 		//Olha as configurações do smtp do seu email.
+		Properties properties = new Properties();
+		
+		properties.put("mail.smtp.auth", "true");// Autorização.
+		properties.put("mail.smtp.starttls", "");// Autenticação.
+		properties.put("mail.smtp.host", "smtp.gmail.com");// Servidor gmail google.
+		properties.put("mail.smtp.port", "465");//Porta do servidor.
+		properties.put("mail.smtp.socketFactory.port", "465");// Expecifica a porta a ser usada pelo socket.
+		properties.put(properties, properties);
+		
 	}
   
 }
