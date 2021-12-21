71ab85cdeaf34027191cf26ec94e525279101f17
Josué <vintetrezi@gmail.com>
2021-12-21 06:31:53 -0300

    Add button.

--- enviar.email/src/test/java/enviar/email/enviar/email/AppTest.java
+++ enviar.email/src/test/java/enviar/email/enviar/email/AppTest.java
@@ -23,7 +23,10 @@ public class AppTest
 		stringBuilderEnviarEmail.append("<h1>Olá<h1/><br/><br/>");
 		stringBuilderEnviarEmail.append("<h2>Você está recebendo acesso ao curso de HTML.<h2/><br/>");
 		stringBuilderEnviarEmail.append("<h4>Você pode ter acesso no botão abaixo.<h4/>");
-		stringBuilderEnviarEmail.append("<a target=\"_blank\" href=\"https://projetojavaweb.com/certificado-aluno/login\">"
+		stringBuilderEnviarEmail.append("<a target=\"_blank\" href=\"https://projetojavaweb.com/certificado-aluno/login\" "
+				+ "style=\" color:#252507; padding: 14px 25px; text-align: center; text-decoration: none;"
+				+ "display: inline-block; border-radius: 30px; font-size: 20px; font-family: courier; border: 3px solid green;"
+				+ "background-color: #99DA39\">"
 				+ "Acesse o portal</a>");
 		
 		ObjetoEnviaEmail enviaEmail = 
